522d3a768a928ab8efaf112252724acd2276e03c
Philippe M <philippe@martinou.net>
2023-06-27 09:12:08 +0200

    Minimal reproduction

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,27 @@
 
   let formHtml: HTMLFormElement;
 
+  let fieldValue = "";
   let valueA = "";
   let valueB = "";
-  let value = "";
+  let useA = true;
 
-  let page = "A";
   const update = () => {
-    if(page === "A") {
-      value = valueA;
-    } else {
-      value = valueB;
-    }
+    fieldValue = useA ? valueA : valueB;
   }
   update();
 
-  const onPageChange = (e:Event) => {
+  const accept = (e:Event) => {
     e.preventDefault();
     // Autosave the value of the current page
     const formData = new FormData(formHtml);
-    const currentVal = formData.get("val") as string;
-    if(page === "A") {
-      valueA = currentVal;
-      page = "B";
+    const val = formData.get("val") as string;
+    if(useA) {
+      valueA = val;
+      useA = false;
     } else {
-      valueB = currentVal;
-      page = "A";
+      valueB = val;
+      useA = true;
     }
     update();
   }
@@ -36,18 +32,39 @@
 
 <main>
   <form bind:this={formHtml}>
-  <div>Current Step : {page} -  ValueA: '{valueA}' -  ValueB: '{valueB}'  - Value : '{value}'</div>
-  <div>
-    Input Field (should be same as Value): <input name="val" value={value} />
-  </div>
-  <div>
-    <button on:click={onPageChange}>Toggle Step</button>
-  </div>
+    <div>Using the A Value : {useA}</div>
+    <div>ValueA: '{valueA}'</div>
+    <div>ValueB: '{valueB}'</div>
+    <div>Field Value (should be reflected in the Input field): '{fieldValue}'</div>
+
+    <div>
+      Input Field (<b>should be same as Field Value</b>): <input name="val" value={fieldValue} />
+    </div>
+
+    <div>
+      <button on:click={accept}>Store Input value in {useA ? 'valueA' : 'valueB'}</button>
+    </div>
   </form>
+  <div class="explain">
+    <u>Bug reproduction</u>: 
+    <br/>
+    Reload the page. Enter 'aaa' in the input field and click on "Store Input value in valueA".
+    <br/>
+    Observe that the value in "Field Value" is (correctly) ""
+    <br/>
+    But that the value in the input field is still (incorrectly) "aaa"
+    <br/>
+    <b>(the &lt;input value=&#123;fieldValue&#125;&gt; field value has not been updated by the assignemnt of fieldValue variable)</b>
+  </div>
 </main>
 
 <style>
   div {
-    padding: 1.5em;
+    padding: 5px;
+    text-align: left;
+  }
+  .explain {
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 80%;
   }
 </style>
